f6789c9c5205c1de0e971d68e823b846e1dca63c
Gennady Potapov <gp@gp-macbook.local>
2018-12-06 15:59:34 +0200

    metadata, changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# aws_cloudwatch Cookbook CHANGELOG
+
+This file is used to list changes made in each version of the aws_cloudwatch cookbook.
+
+## 0.1.6 (2018-12-06)
+- Initial release
--- metadata.rb
+++ metadata.rb
@@ -1,19 +1,15 @@
 name 'aws_cloudwatch'
 maintainer 'Gennady Potapov'
 license 'Apache-2.0'
-description 'Installs/Configures AWS Cloudwatch Agent'
-long_description 'Installs/Configures aws_cloudwatch'
-version '0.1.5'
+description 'Provides aws_cloudwatch_agent resource'
+version '0.1.6'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-issues_url 'https://github.com/gp42/aws_cloudwatch/issues'
+supports 'ubuntu', '= 14.04'
+supports 'ubuntu', '= 16.04'
+supports 'centos'
+supports 'fedora'
+supports 'amazon'
 
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
+issues_url 'https://github.com/gp42/aws_cloudwatch/issues'
 source_url 'https://github.com/gp42/aws_cloudwatch'
