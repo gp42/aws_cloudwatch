f7f3e2f5754407d2cf6aa56a37b91602628714ad
Gennady Potapov <gennady@grabcad.com>
2022-06-30 01:09:33 +0300

    meta

--- metadata.rb
+++ metadata.rb
@@ -5,9 +5,11 @@ description 'Provides aws_cloudwatch_agent resource'
 version '2.0.0'
 chef_version '>= 13.0' if respond_to?(:chef_version)
 
-supports 'ubuntu', '= 14.04'
 supports 'ubuntu', '= 16.04'
-supports 'centos'
+supports 'ubuntu', '= 18.04'
+supports 'ubuntu', '= 20.04'
+supports 'debian', '>= 9'
+supports 'centos', '= 7'
 supports 'fedora'
 supports 'amazon'
 
